1016950b9ad73f09231835903768fa86d6869e1f
Oleg <kharenkoleg@gmail.com>
2023-11-05 19:50:54 +0100

    comm

--- Version1/src/main/java/SecondHandoutAboutJDBC.java
+++ Version1/src/main/java/SecondHandoutAboutJDBC.java
@@ -33,21 +33,50 @@ public class SecondHandoutAboutJDBC {
 						+ "WHERE exp > ? AND exp < ?");
 				
 				// 2nd -> Parametrization
-				pS.setInt(1, 1); // 1 = 1st parameters, value 1200
-				pS.setInt(2, 5); // 2 = 2nd parameter, value 1300
+				pS.setInt(1, 1); // 1 = 1st parameters, value ...
+				pS.setInt(2, 5); // 2 = 2nd parameter, value ...
 				
 				// 3rd -> Execution
 				int n = pS.executeUpdate(); // 
 				if (n != 0) System.out.println("GOOD UPDATE");
 				else System.out.println("BAD UPDATE");
 				
+				// --- EXERCISE 1 ---
+				 // 1st -> Preparing the SELECT query with placeholders
+                String sSQL = "SELECT * FROM alumnos WHERE exp >= ?";
+
+                // 2nd -> Creating the PreparedStatement
+                pS = con.prepareStatement(sSQL);
+				for (int i = 1; i <= 3; i++) {
+                    pS.setInt(1, i); // Set the parameter value
+                    ResultSet resultSet = pS.executeQuery();
+                    System.out.println("Results for exp > " + i + ":");
+                    ShowResults(resultSet);
+                }
+				
 			}
 		}
-		catch (Exception ex){
+		catch (Exception e){
 			System.out.println("EXCEPTION: CONNECTION NOT ESTABLISHED!");
-			ex.printStackTrace(); // Showing the TRACEBACK of our EXCEPTION
+			e.printStackTrace(); // Showing the TRACEBACK of our EXCEPTION
 		}
 
+	}
+	
+	public static void ShowResults(ResultSet results) {
+		try {
+			while (results.next()) {
+				int exp = results.getInt("exp");
+				String nombre = results.getString("nombre");
+	            String nota = results.getString("nota");
+	            System.out.println("Exp: " + exp + " | Nombre: " + nombre + " | Nota: " + nota);
+			}
+		}
+		catch (Exception e) {
+			System.out.println("EXCEPTION: ERROR IN METHOD ShowResults");
+			e.printStackTrace(); // Showing the TRACEBACK of our EXCEPTION
+		}
+		
 	}
 
 }
