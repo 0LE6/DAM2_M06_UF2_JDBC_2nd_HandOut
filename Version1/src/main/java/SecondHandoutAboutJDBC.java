dd7041b2a41f7648ae182aacb9774e6849c04dd2
Oleg <kharenkoleg@gmail.com>
2023-11-05 20:54:38 +0100

    comm import *

--- Version1/src/main/java/SecondHandoutAboutJDBC.java
+++ Version1/src/main/java/SecondHandoutAboutJDBC.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 public class SecondHandoutAboutJDBC {
 
