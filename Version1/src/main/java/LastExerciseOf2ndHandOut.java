f0678919c38a552651fc1f2bb9287158c15b431d
Oleg <kharenkoleg@gmail.com>
2023-11-05 22:38:56 +0100

    comm --

--- Version1/src/main/java/LastExerciseOf2ndHandOut.java
+++ Version1/src/main/java/LastExerciseOf2ndHandOut.java
@@ -14,6 +14,7 @@ public class LastExerciseOf2ndHandOut {
 		
 		String connectionUrl = "jdbc:mysql://localhost:3306/hospital?serverTimezone=UTC";
 		String user = "root"; String pass = "";
+		CallableStatement cS = null;
 		boolean everythingIsOK = true;
 		
 		try (Connection con = DriverManager.getConnection(connectionUrl, user, pass);) {
@@ -21,14 +22,28 @@ public class LastExerciseOf2ndHandOut {
 			con.setAutoCommit(false);
 			
 			try {
-				// ...
-				// ...
+				
+				// 1st - We will call our created PROCEDURE in phpMyadmin
+				// to create our new doctor using parametrization (Is correctly written?)
+				cS = con.prepareCall("{call CreateDoctor(?, ?, ?, ?)}");
+				cS.setInt(1, d.getDoctorCode());
+				cS.setString(2, d.getName());
+				cS.setString(3, d.getSpecialization());
+				cS.setInt(4, d.getHospitalCode());
+				cS.execute();
+
+	            // Llama al procedimiento almacenado para actualizar el código del hospital
+				cS = con.prepareCall("{call UpdateDoctorHospital(?, ?)}");
+				cS.setInt(1, d.getDoctorCode());
+				cS.setInt(2, updateHospitalCode);
+				cS.execute();
 				
 				con.commit();
 				System.out.println("Transaction committed succesfully!");
 				
 			}
 			catch (SQLException e){
+				everythingIsOK = false;
 				con.rollback();
 				System.out.println("Transaction rolled back due to an Exception");
 				e.printStackTrace(); // Showing the TRACEBACK of our EXCEPTION
