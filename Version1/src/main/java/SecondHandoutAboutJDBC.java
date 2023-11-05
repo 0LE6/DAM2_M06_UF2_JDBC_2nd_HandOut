53e8c6d903fcdee3b51bf4fc5cceb9e62bbfca46
Oleg <kharenkoleg@gmail.com>
2023-11-05 19:18:07 +0100

    starting w/ PreparedStatement

--- Version1/src/main/java/SecondHandoutAboutJDBC.java
+++ Version1/src/main/java/SecondHandoutAboutJDBC.java
@@ -24,6 +24,10 @@ public class SecondHandoutAboutJDBC {
 //				rS.close();
 //				st.close();
 //				conn.close();
+				
+				// PREPARED STATEMENT from here:
+				
+				
 			}
 		}
 		catch (Exception ex){
